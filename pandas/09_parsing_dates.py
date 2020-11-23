f2863a820b3c1841c4eeff494f266b9792772412
Neutrollized <glen.yu@gmail.com>
2020-11-23 15:55:57 -0500

    parsing dates

---
+++ pandas/09_parsing_dates.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime
+
+np.random.seed(0)
+
+
+landslides = pd.read_csv("../data/landslides_data.csv")
+
+print(landslides.columns)
+print(landslides.shape)
+print(landslides)
+print(landslides.isnull().sum())
+
+print(landslides.groupby('country_name').country_name.count())
+
+print(landslides['date'].head())
+print(landslides['date'].dtype)
+
+# i.e. 3/22/07 --> 2007-03-22
+landslides['date_parsed'] = pd.to_datetime(landslides['date'], format="%m/%d/%y")
+print(landslides['date_parsed'])
+print(landslides['date_parsed'].dtype)
+
+# plot graph of the landslides by month
+landslides_months = landslides['date_parsed'].dt.month
+landslides_months = landslides_months.dropna()
+sns.distplot(landslides_months, kde=False, bins=12)
+plt.show()
+
+# plot graph of the landslides by day of month
+landslides_days = landslides['date_parsed'].dt.day
+landslides_days = landslides_days.dropna()
+sns.distplot(landslides_days, kde=False, bins=31)
+plt.show()
