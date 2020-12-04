28a99efe033486a570258ad8f07d1e83703f5b28
Neutrollized <glen.yu@gmail.com>
2020-12-03 22:26:42 -0500

    inconsisten data handling

--- pandas/10_char_encoding.py
+++ pandas/10_char_encoding.py
@@ -19,20 +19,33 @@ utf8 = before.encode("utf-8", errors="replace")
 print(utf8)
 print(type(utf8))
 
+# We've lost the original underlying byte string! It's been 
+# replaced with the underlying byte string for the unknown character :(
 ascii = before.encode("ascii", errors="replace")
 print(ascii)
 print(type(ascii))
+
+# you can go backwards with decode (c'mon, of course there's a decode!)
+new_before = utf8.decode("utf-8")
+print(new_before)
 
 
 #-----------------------------
 # Kickstarter example
 #-----------------------------
 # reading this file will give you errors with 
-#kickstarter2016 = pd.read_csv("../data/ks-projects-201612.csv.zip")
+#kickstarter2016 = pd.read_csv("../data/ks-projects-201612.csv")
 
 # NOTE: here, your data file needs to be unzipped ('open' is not as smart as 'read_csv')
+# you may also want to play around with the number of bytes you read as 10k bytes might give you the wrong answer
 with open("../data/ks-projects-201612.csv", 'rb') as rawdata:
   result = chardet.detect(rawdata.read(10000))
 
 print(result)
-#print(kickstarter2016.columns)
+
+
+kickstarter2016 = pd.read_csv("../data/ks-projects-201612.csv", encoding='Windows-1252')
+print(kickstarter2016.columns)
+
+# you can then opt to write it back out in a more standard UTF-8 format for easier future use
+#kickstarter2016.to_csv("ks-projects-201612-utf8.csv")
---
+++ pandas/11_inconsistent_data.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+# helpful modules
+import fuzzywuzzy
+from fuzzywuzzy import process
+import chardet
+
+np.random.seed(0)
+
+
+#--------------------------------
+# Import data 
+#--------------------------------
+profs = pd.read_csv("../data/pakistan_intellectual_capital.csv")
+
+print(profs.head())
+print(profs.describe)
+countries = profs['Country'].unique()
+countries.sort()
+print(countries)
+
+
+#---------------------------
+# Fix inconsistencies
+#---------------------------
+# convert to lower case
+profs['Country'] = profs['Country'].str.lower()
+profs['Country'] = profs['Country'].str.strip()
+
+countries = profs['Country'].unique()
+countries.sort()
+print(countries)
+
+
+# https://chairnerd.seatgeek.com/fuzzywuzzy-fuzzy-string-matching-in-python/
+#sk_matches = fuzzywuzzy.process.extract("south korea", countries, limit=10, scorer=fuzzywuzzy.fuzz.token_sort_ratio)
+#print(sk_matches)
+
+#usa_matches = fuzzywuzzy.process.extract("usa", countries, limit=10, scorer=fuzzywuzzy.fuzz.token_sort_ratio)
+#print(usa_matches)
+
+# function to replace rows in the provided column of the provided dataframe
+# that match the provided string above the provided ratio with the provided string
+def replace_matches_in_column(df, column, string_to_match, min_ratio = 47):
+  # get a list of unique strings
+  strings = df[column].unique()
+    
+  # get the top 10 closest matches to our input string
+  matches = fuzzywuzzy.process.extract(string_to_match, strings, 
+                                       limit=10, scorer=fuzzywuzzy.fuzz.token_sort_ratio)
+
+  # only get matches with a ratio > min_ratio
+  close_matches = [matches[0] for matches in matches if matches[1] >= min_ratio]
+
+  # get the rows of all the close matches in our dataframe
+  rows_with_matches = df[column].isin(close_matches)
+
+  # replace all rows with close matches with the input matches 
+  df.loc[rows_with_matches, column] = string_to_match
+    
+  # let us know the function's done
+  print("All done!")
+
+
+replace_matches_in_column(df=profs, column='Country', string_to_match="south korea")
+replace_matches_in_column(df=profs, column='Country', string_to_match="usa")
+
+countries = profs['Country'].unique()
+countries.sort()
+print(countries)
